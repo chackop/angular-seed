ef3d724ff50333c55bbaf57e16d02dbaa09159bf
chax <chackop@gmail.com>
2017-04-01 12:50:02 +0100

    Updated docs, more info
    Updated docs, more info

--- app/app.js
+++ app/app.js
@@ -6,8 +6,8 @@ angular.module('myApp', [
   'myApp.version'
 ])
   .controller('weatherController', ['$http', '$scope', function ($http, $scope) {
-    // var vm = $this; 
     var URL = 'http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&cnt=5&appid=76ecf50a3b54ae7a236fab542e340c50';
+    // Structured request header
     // var URL = 'http://api.openweathermap.org/data/2.5/forecast/'; 
     // var request = {
     //   method: 'GET',
@@ -23,6 +23,7 @@ angular.module('myApp', [
 
     $http.get(URL)
       .success(function (response) {
-        $scope.tempdata = response.list;
+        // take basic response list
+        $scope.weatherdata = response.list;
       });
   }]);
